6ea2949838f3ffc6994fca2bf62ef1e32ba9a625
Your Name <you@example.com>
2024-07-25 14:41:15 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import React from 'react';
 import './App.css';
+import { useState } from 'react';
+import Counter from './components/Counter';
 
 function App() {
+  const [likes, setLikes] = useState(1)
+  const [value, setValue] = useState('text')
+
+  function increment() {
+    setLikes(likes + 1)
+  }
+
+  function decrement() {
+    setLikes(likes - 1)
+  }
+
   return (
     <div className="App">
-      <h1>help</h1>
-    </div>
+      <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input
+        type='text'
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      />
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+      <Counter />
+    </div >
   );
 }
 
